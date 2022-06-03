7469e4be2050ce9c445892f3eca7abc03a0e86de
Chris Livingston <clivingston45@gmail.com>
2022-06-03 12:22:08 -0700

    Deploy React app to GitHub Pages

---
+++ precache-manifest.bfd5f21d526d11faaaee668c6442d3c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4080aa27acc5b8b8f69",
+    "url": "/ais-ecommerce-demo/static/css/main.ebd73403.chunk.css"
+  },
+  {
+    "revision": "d4080aa27acc5b8b8f69",
+    "url": "/ais-ecommerce-demo/static/js/main.d4080aa2.chunk.js"
+  },
+  {
+    "revision": "26ccb0c19ce2957b908b",
+    "url": "/ais-ecommerce-demo/static/js/1.26ccb0c1.chunk.js"
+  },
+  {
+    "revision": "759ea99b1ef035799d67",
+    "url": "/ais-ecommerce-demo/static/js/runtime~main.759ea99b.js"
+  },
+  {
+    "revision": "243083745e1dd5b7e3693a2df2492a10",
+    "url": "/ais-ecommerce-demo/index.html"
+  }
+];
